38c7d858de06b8efd65974116c44720337d9797f
MEARZIM <asahaayan@gmail.com>
2024-08-25 19:26:09 +0530

    fix: doctor table size problem

--- sql/doctor.sql
+++ sql/doctor.sql
@@ -3,7 +3,7 @@ CREATE TABLE cms.Doctors (
     dUserName VARCHAR(50) NOT NULL PRIMARY KEY,
     doctorName VARCHAR(50) NOT NULL,
     doctorSpecialist VARCHAR(50) NOT NULL,
-    doctorContacts INT,
+    doctorContacts BIGINT,
     doctorEmail VARCHAR(50) NOT NULL UNIQUE,
     doctorPassword VARCHAR(50) NOT NULL,
     doctorFess INT NOT NULL,
@@ -11,3 +11,5 @@ CREATE TABLE cms.Doctors (
     doctorDescription VARCHAR(500),
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+ALTER TABLE `doctors` CHANGE `doctorContacts` `doctorContacts` BIGINT(11) NULL DEFAULT NULL;
